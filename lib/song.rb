dd2ebe6e689f6559134afc64065a123061c78a50
Matt D <dayclawtel@gmail.com>
2021-03-04 18:22:34 -0800

    commit tree 6453a9955b94f08aa918c73bbfe518299770814d

--- lib/song.rb
+++ lib/song.rb
@@ -1,5 +1,6 @@
 class Song
   attr_accessor :name, :artist_name
+
   @@all = []
 
   def self.all
@@ -7,7 +8,67 @@ class Song
   end
 
   def save
-    self.class.all << self
+    self.class.all.push self
+  end
+
+  def self.create
+    song = self.new
+    song.save
+    song
+  end
+
+  def self.new_by_name(name)
+    song = self.new
+    song.name = name
+    song
+  end
+
+  def self.create_by_name(name)
+    song = self.create
+    song.name = name
+    song
+  end
+
+  def self.find_by_name(name)
+    @@all.detect { |song| song.name == name }
+  end
+
+  def self.find_or_create_by_name(name)
+    # if self.find_by_name(name)
+    # self.find_by_name(name)
+    # else
+    # self.create_by_name(name)
+    self.find_by_name(name) || self.create_by_name(name)
+  end
+
+  def self.alphabetical
+    # @@all.sort(name)
+    # @@all.sort{ |a, b| a.name <=> b.name }
+    @@all.sort_by{ |song| song.name }
+  end
+
+  def self.new_from_filename(filename)
+    new_file = filename.split(" - ")
+    artist_name = new_file[0]
+    song_name = new_file[1].gsub(".mp3","")
+    song = self.new
+    song.name = song_name
+    song.artist_name = artist_name
+    song
+  end
+
+  def self.create_from_filename(filename)
+    new_file = filename.split(" - ")
+    artist_name = new_file[0]
+    song_name = new_file[1].gsub(".mp3","")
+    song = self.create
+    song.name = song_name
+    song.artist_name = artist_name
+    song
+  end
+
+  def self.destroy_all
+    self.all.clear
   end
 
 end
